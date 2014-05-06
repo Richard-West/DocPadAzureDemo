1086ffb15aba9e0975b850c363d3da8409c0e4a0
Richard West <rwest@peachtreedata.com>
2014-05-06 11:45:57 -0400

    Changed deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -86,27 +86,29 @@ goto :EOF
 :: ----------
 
 :Deployment
-echo Handling DocPad deployment.
+echo Handling node.js deployment.
 
-:: 1. Select node version
+:: 1. KuduSync
+IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: 2. Select node version
 call :SelectNodeVersion
 
-:: 2. Install npm packages
-echo Installing npm packages...
-pushd "%DEPLOYMENT_SOURCE%"
-call !NPM_CMD! install --production
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
 
-:: 3. Build DocPad Site
+:: 4. Build DocPad Site
 echo Building the DocPad site
 pushd %DEPLOYMENT_TARGET%
 call  %DEPLOYMENT_TARGET%\node_modules\.bin\docpad.cmd generate
-IF !ERRORLEVEL! NEQ 0 goto error
-
-:: 3. KuduSync
-echo Copying Files...
-call %KUDU_SYNC_CMD% -v 500 -f "%DEPLOYMENT_SOURCE%\out" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
---
+++ src/documents/pages/test.html.md
@@ -0,0 +1,83 @@
+```
+title: This is an example
+layout: example
+tags: ['intro','page']
+pageOrder: 1
+```
+
+
+
+This is a **page**
+
+##This is a **page**
+
+This is a **page**
+
+###This is a **page**
+
+This is a **page**
+
+#This is a **page**
+
+This is a **page**
+
+##This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+This is a **page**
+
+Hello world!
